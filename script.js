124b10118f8dd1e57f7c4d9449cc7885beef3298
Akshit Singh <akshitsingh254@gmail.com>
2026-03-27 12:29:02 +0530

    Added functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.querySelector(".display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+function backSpace() {
+    display.value = display.value.slice(0, -1);
+}
