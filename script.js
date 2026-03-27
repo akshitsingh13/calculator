b57cffefa4666776345bb56beae465c08bb5be00
Akshit Singh <akshitsingh254@gmail.com>
2026-03-27 12:38:32 +0530

    Added keydown events

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function calculate() {
 function backSpace() {
     display.value = display.value.slice(0, -1);
 }
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Backspace') {
+        backSpace();
+        return;
+    }
+    if (e.key === 'Enter') {
+        calculate();
+        return;
+    }
+    if (e.key === 'Escape') {
+        clearDisplay();
+        return;
+    }
+    const btn = document.getElementById(e.key);
+    if (btn) btn.click();
+})
